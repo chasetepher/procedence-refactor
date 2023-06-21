b3aaa8a91c16c74ea5e075099cbf8dd857d271b0
chasetepher <criggstx@gmail.com>
2023-06-20 20:46:21 -0500

    refactored theFront to Procedence level Lorum Ipsum

---
+++ react-scripts/src/views/IndexView/components/Features/Features.js
@@ -0,0 +1,109 @@
+/* eslint-disable react/no-unescaped-entities */
+import React, { useState } from 'react';
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
+import { useTheme } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Box from '@mui/material/Box';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+const mock = [
+  {
+    title: 300,
+    subtitle: '300 emails saved each week by switching to carrier pigeon',
+    suffix: '+',
+  },
+  {
+    title: 45,
+    subtitle: '45 years of doing this work',
+    suffix: '+',
+  },
+  {
+    title: 99,
+    subtitle: '99% of our customers are still in business',
+    suffix: '%',
+  },
+];
+
+const Features = () => {
+  const theme = useTheme();
+  const isMd = useMediaQuery(theme.breakpoints.up('md'), {
+    defaultMatches: true,
+  });
+
+  const [viewPortEntered, setViewPortEntered] = useState(false);
+  const setViewPortVisibility = (isVisible) => {
+    if (viewPortEntered) {
+      return;
+    }
+
+    setViewPortEntered(isVisible);
+  };
+
+  return (
+    <Box>
+      <Grid container spacing={4} direction={isMd ? 'row' : 'column-reverse'}>
+        <Grid item xs={12} md={6}>
+          <Box marginBottom={4}>
+            <Typography sx={{ fontWeight: 700 }} variant={'h4'} gutterBottom>
+              Why We Do What We Do{' '}
+            </Typography>
+            <Typography variant={'h6'} component={'p'} color={'text.secondary'}>
+              As a nation, our competitive edge has always been the ability to
+              adapt to new technology. This same virtue must remain to maintain
+              our global leadership position.
+            </Typography>
+          </Box>
+          <Grid container spacing={2}>
+            {mock.map((item, i) => (
+              <Grid key={i} item xs={12} md={4}>
+                <Typography variant="h4" color={'primary'} gutterBottom>
+                  <VisibilitySensor
+                    onChange={(isVisible) => setViewPortVisibility(isVisible)}
+                    delayedCall
+                  >
+                    <CountUp
+                      duration={2}
+                      end={viewPortEntered ? item.title : 0}
+                      start={0}
+                      suffix={item.suffix}
+                    />
+                  </VisibilitySensor>
+                </Typography>
+                <Typography color="text.secondary" component="p">
+                  {item.subtitle}
+                </Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          justifyContent="center"
+          alignItems="center"
+          xs={12}
+          md={6}
+          sx={{
+            display: { xs: 'none', md: 'flex' },
+          }}
+        >
+          <Box component={Card} boxShadow={4} height={1} width={1}>
+            <Box
+              component={CardMedia}
+              height={1}
+              width={1}
+              minHeight={300}
+              image="https://assets.maccarianagency.com/backgrounds/img4.jpg"
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Features;
---
+++ react-scripts/src/views/IndexView/components/Services/Sevices.js
@@ -0,0 +1,148 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import { alpha, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Grid';
+
+const mock = [
+  {
+    title: 'Learn',
+    subtitle:
+      'Our learning and performance specialists help companies, big and small, discover how to get the best possible return on their technology and personnel investments to differentiate themselves from the competition.',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: 'Assess',
+    subtitle:
+      'We do this by assessing current workflow process, making technology-based recommendations and providing post evaluation of progress and usage of solutions.',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: 'Propel',
+    subtitle:
+      'Not only do our findings, recommendations and follow-up propel our clients’ business, it helps us grow, adapt and constantly provide more efficient solutions.',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+        />
+      </svg>
+    ),
+  },
+];
+
+const Services = () => {
+  const theme = useTheme();
+  return (
+    <Box>
+      <Box marginBottom={4}>
+        <Box marginBottom={2}>
+          <Typography
+            variant="h4"
+            color="text.primary"
+            align={'center'}
+            gutterBottom
+            sx={{
+              fontWeight: 700,
+            }}
+          >
+            Why We Excel at Procedence
+          </Typography>
+          <Typography
+            variant="h6"
+            component="p"
+            color="text.secondary"
+            sx={{ fontWeight: 400 }}
+            align={'center'}
+          >
+            Procedence improves your bottom line and competitive edge by helping
+            your people adapt to new technology. That technology may be
+            software, hardware or even new practices that change the way your
+            company fundamentally operates.
+          </Typography>
+        </Box>
+      </Box>
+      <Grid container spacing={2}>
+        {mock.map((item, i) => (
+          <Grid item xs={12} md={4} key={i}>
+            <Box width={1} height={1}>
+              <Box
+                display={'flex'}
+                flexDirection={'column'}
+                alignItems={'center'}
+              >
+                <Box
+                  component={Avatar}
+                  width={60}
+                  height={60}
+                  marginBottom={2}
+                  bgcolor={alpha(theme.palette.primary.main, 0.1)}
+                  color={theme.palette.primary.main}
+                >
+                  {item.icon}
+                </Box>
+                <Typography
+                  variant={'h6'}
+                  gutterBottom
+                  sx={{ fontWeight: 500 }}
+                  align={'center'}
+                >
+                  {item.title}
+                </Typography>
+                <Typography align={'center'} color="text.secondary">
+                  {item.subtitle}
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default Services;
